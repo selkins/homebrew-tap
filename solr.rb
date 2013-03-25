dce0d93a1bd6bd6ef61cc07f9af93d3043e77a9a
Joon Park <joon@dvessel.com>
2013-03-25 13:13:07 -0400

    Solr 3.6.2

---
+++ solr.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+class Solr < Formula
+  homepage 'http://lucene.apache.org/solr/'
+  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/3.6.2/apache-solr-3.6.2.tgz'
+  sha1 '3a1a40542670ea6efec246a081053732c5503ec1'
+
+  def script; <<-EOS.undent
+    #!/bin/sh
+    if [ -z "$1" ]; then
+      echo "Usage: $ solr path/to/config/dir"
+    else
+      cd #{libexec}/example && java -Dsolr.solr.home=$1 -jar start.jar
+    fi
+    EOS
+  end
+
+  def install
+    libexec.install Dir['*']
+    (bin+'solr').write script
+  end
+
+  def caveats; <<-EOS.undent
+    To start solr:
+      solr path/to/solr/config/dir
+
+    See the solr homepage for more setup information:
+      brew home solr
+    EOS
+  end
+end
